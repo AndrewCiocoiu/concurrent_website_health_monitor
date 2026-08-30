8aa1b2f1501a775f315cbb8299f10a3f3cdc82f7
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-08-30 22:06:52 +0300

    Implement channels to respect go's concurrency motto that We dont communicate by sharing memory, instead we share memory by communicating

--- main.go
+++ main.go
@@ -9,17 +9,20 @@ import (
 	"sync"
 )
 
-func checkUrl(url string, client *http.Client, wg *sync.WaitGroup) {
+func checkUrl(url string, client *http.Client, wg *sync.WaitGroup, ch chan string) {
 	defer wg.Done()
 
 	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Printf("%s - DOWN\n", url)
+		res := fmt.Sprintf("%s - DOWN\n", url)
+		ch <- res
 		return
 	} else if resp.StatusCode == 200 {
-		fmt.Printf("%s - UP\n", url)
+		res := fmt.Sprintf("%s - UP\n", url)
+		ch <- res
 	} else {
-		fmt.Printf("%s - DOWN\n", url)
+		res := fmt.Sprintf("%s - DOWN\n", url)
+		ch <- res
 	}
 
 	resp.Body.Close()
@@ -27,6 +30,8 @@ func checkUrl(url string, client *http.Client, wg *sync.WaitGroup) {
 
 func main() {
 	var wg sync.WaitGroup
+	ch := make(chan string)
+	defer close(ch)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("You forgot to enter the filename as a CLI argument! Please do so!")
@@ -48,11 +53,15 @@ func main() {
 		url := scanner.Text()
 
 		wg.Add(1)
-		go checkUrl(url, client, &wg)
+		go checkUrl(url, client, &wg, ch)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("There was an error while scanning the file: %s\n", err)
+	}
+
+	for val := range ch {
+		fmt.Printf("%s", val)
 	}
 
 	wg.Wait()
