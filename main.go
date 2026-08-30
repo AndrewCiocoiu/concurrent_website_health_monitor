3c33252e4217f2ac12865f33e5abae160aa33a9e
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-08-30 20:26:08 +0300

    Implement reading the opened file line by line

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -11,4 +13,15 @@ func main() {
 		log.Fatalf("Failed to open file: %s\n", err)
 	}
 	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("%s\n", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("There was an error while scanning the file: %s\n", err)
+	}
 }
