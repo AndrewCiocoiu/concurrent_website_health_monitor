f870a539bf5d7a1e78b25957e60731db00b2d60c
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-08-31 10:05:43 +0300

    Implement worker pools to prevent socket and file descriptor overload and potential ip bans

--- main.go
+++ main.go
@@ -15,30 +15,34 @@ type WebsiteStatus struct {
 	status string
 }
 
-func checkUrl(url string, client *http.Client, wg *sync.WaitGroup, ch chan WebsiteStatus) {
+func worker(jobs chan string, client *http.Client, wg *sync.WaitGroup, ch chan WebsiteStatus) {
 	defer wg.Done()
-	resp, err := client.Get(url)
 
-	new_website := WebsiteStatus{url: url}
+	for url := range jobs {
+		resp, err := client.Get(url)
 
-	if err != nil {
-		new_website.status = "DOWN"
-		ch <- new_website
-		return
-	} else if resp.StatusCode == 200 {
-		new_website.status = "UP"
-		ch <- new_website
-	} else {
-		new_website.status = "DOWN"
-		ch <- new_website
+		new_website := WebsiteStatus{url: url}
+
+		if err != nil {
+			new_website.status = "DOWN"
+			ch <- new_website
+			continue
+		} else if resp.StatusCode == 200 {
+			new_website.status = "UP"
+			ch <- new_website
+		} else {
+			new_website.status = "DOWN"
+			ch <- new_website
+		}
+
+		resp.Body.Close()
 	}
-
-	resp.Body.Close()
 }
 
 func main() {
 	var wg sync.WaitGroup
 	ch := make(chan WebsiteStatus)
+	jobs := make(chan string)
 
 	if len(os.Args) < 2 {
 		log.Fatalf("You forgot to enter the filename as a CLI argument! Please do so!")
@@ -54,14 +58,20 @@ func main() {
 
 	client := &http.Client{Timeout: 5 * time.Second}
 
+	//Launch 15 workers to prevent socket exhaustion
+	for i := 0; i < 15; i++ {
+		go worker(jobs, client, &wg, ch)
+		wg.Add(1)
+	}
+
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		url := scanner.Text()
-
-		wg.Add(1)
-		go checkUrl(url, client, &wg, ch)
+		jobs <- url
 	}
+	//Close the jobs channel as soon as I put all the jobs in so that my workers dont become deadlocked when they finnish all jobs
+	close(jobs)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("There was an error while scanning the file: %s\n", err)
